5f1b3b02f847841c4acc38b24792bfbd5a5408af
TheMagician001 <105514676+TheMagician001@users.noreply.github.com>
2022-06-22 01:47:51 -0400

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-list';
+
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'Program', done: true },
+    { description: 'Work Out', done: true },
+    { description: 'read with Maria', done: false },
+    { description: 'Train with Ricky', done: false },
+    { description: 'tell caroline i love her', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter(item => this.filter === 'done' ? item.done : !item.done);
+  }
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false
+    });
+  }
+  
+  // remove(item) {
+  //   this.allItems.splice(this.allItems.indexOf(item), 1);
+  // }
+  
+}
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,17 @@
+// import { Component, Input, Output, EventEmitter } from '@angular/core';
+// import { Item } from "../item";
+// export class ItemComponent {
+
+  
+//   editable = false;
+  
+//   @Input() item: Item;
+//   @Input() newItem: string;
+//   @Output() remove = new EventEmitter<Item>();
+  
+//   saveItem(description) {
+//     if (!description) return;
+//     this.editable = false;
+//     this.item.description = description;
+//   }
+// }
