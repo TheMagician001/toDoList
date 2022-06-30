eb8da96eb4552b22e34c6855c6d8ec69606f5f5c
TheMagician001 <105514676+TheMagician001@users.noreply.github.com>
2022-06-29 23:58:22 -0400

    Add files via upload

--- src/app/itemservice.service.ts
+++ src/app/itemservice.service.ts
@@ -24,5 +24,14 @@ public remove(item:Item) {
   this.allItems.splice(this.allItems.indexOf(item), 1);
 }
 
-}
 
+public duplicateItem(description: string) {
+  this.allItems.push({
+    description,
+    done: false
+  });
+
+
+}
+ 
+}
